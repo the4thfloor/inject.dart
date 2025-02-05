83bfab4e5432fbbaacba03d7421cab165ab37cc6
Ralph Bergmann <ralph@dasralph.de>
2025-02-05 15:44:46 +0100

    disable generation of localized Strings from arb files

--- examples/flutter_skeleton/lib/src/app.dart
+++ examples/flutter_skeleton/lib/src/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:inject_annotation/inject_annotation.dart';
 
+import 'localization/app_localizations.dart';
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
@@ -57,7 +57,7 @@ class MyApp extends StatelessWidget {
           //
           // The appTitle is defined in .arb files found in the localization
           // directory.
-          onGenerateTitle: (BuildContext context) => AppLocalizations.of(context)!.appTitle,
+          onGenerateTitle: (BuildContext context) => AppLocalizations.of(context).appTitle,
 
           // Define a light and dark color theme. Then, read the user's
           // preferred ThemeMode (light, dark, or system default) from the
